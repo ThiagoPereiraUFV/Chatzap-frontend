0806874cd12703240244dc84481add4e87cac2d9
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2021-03-23 15:52:49 -0300

    Animating page transitions

--- src/pages/Website/NotFound/index.js
+++ src/pages/Website/NotFound/index.js
@@ -1,18 +1,28 @@
-import { Jumbotron } from "react-bootstrap";
 import { Link } from "react-router-dom";
+
+import { Jumbotron } from "react-bootstrap";
+
+import { motion } from "framer-motion";
 
 //	Exporting Not Found page
 export const NotFound = () => (
-	<Jumbotron  style={{borderRadius: "30px"}} className="my-auto ml-4 mr-auto bg-success" sm="7">
-		<h2 className="display-5 font-italic">ERRO 404</h2>
-		<h1 className="display-1 text-light font-weight-bold my-auto">Não encontrado</h1>
-		<hr className="my-3"/>
-		<p className="lead">
-			<Link
-				to="/"
-				className="btn btn-outline-light btn-lg px-3 py-1">
-				Voltar
-			</Link>
-		</p>
-	</Jumbotron>
+	<motion.div
+		className="d-flex flex-row flex-wrap justify-content-around my-auto"
+		initial={{ opacity: 0 }}
+		exit={{ opacity: 0 }}
+		animate={{ opacity: 1 }}
+	>
+		<Jumbotron  style={{borderRadius: "30px"}} className="my-auto ml-4 mr-auto bg-success" sm="7">
+			<h2 className="display-5 font-italic">ERRO 404</h2>
+			<h1 className="display-1 text-light font-weight-bold my-auto">Não encontrado</h1>
+			<hr className="my-3"/>
+			<p className="lead">
+				<Link
+					to="/"
+					className="btn btn-outline-light btn-lg px-3 py-1">
+					Voltar
+				</Link>
+			</p>
+		</Jumbotron>
+	</motion.div>
 );
