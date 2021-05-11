9fab6bbb79ecea54924dba05a022d17e9ec66775
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2021-05-11 20:33:47 -0300

    Adding style to chat navbar

--- src/components/Infobar/index.js
+++ src/components/Infobar/index.js
@@ -1,23 +1,38 @@
 import { Link, useHistory } from "react-router-dom";
 
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, Accordion, Card, Button } from "react-bootstrap";
 
-import { RiCloseFill, RiLogoutCircleLine } from "react-icons/ri";
+import { RiArrowLeftLine, RiLogoutCircleLine } from "react-icons/ri";
 
 export const Infobar = {
 	Chat: ({ room }) => {
 		const history = useHistory();
 
 		return (
-			<Navbar className="m-0" bg="success" variant="light" sticky="top">
-				<Nav className="mr-auto">
-					<Nav.Item>
-						<Navbar.Brand className="text-light font-weight-bold">{room}</Navbar.Brand>
-					</Nav.Item>
+			<Navbar className="m-0 p-0" bg="success" variant="light" sticky="top">
+				<Nav>
+					<Button className="rounded-0" variant="success" size="lg" onClick={() => history.push("/chat")}>
+						<RiArrowLeftLine className="text-white" size="25" />
+					</Button>
 				</Nav>
-				<Nav className="ml-auto">
-					<Nav.Item className="btn" onClick={() => history.push("/chat")}>
-						<RiCloseFill className="text-white" size="25" />
+				<Nav className="w-100">
+					<Nav.Item className="w-100">
+						<Accordion>
+							<Card bg="success">
+								<Accordion.Toggle
+									as={Button}
+									className="text-left font-weight-bold rounded-0 py-2"
+									variant="success"
+									size="lg"
+									eventKey="0"
+								>
+        					{room}
+								</Accordion.Toggle>
+								<Accordion.Collapse eventKey="0">
+									<Card.Body>Hello! Im the body</Card.Body>
+								</Accordion.Collapse>
+							</Card>
+						</Accordion>
 					</Nav.Item>
 				</Nav>
 			</Navbar>
