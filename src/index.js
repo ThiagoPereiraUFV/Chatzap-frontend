12d0f2281f4767d9cffad4d5e7cf0571beef5b70
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2021-07-05 09:00:46 -0300

    Fixing name and version

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import ReactDOM from "react-dom";
 import { App } from "./App";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
+import packageJson from "../package.json";
 
 //	Setting up sentry
 Sentry.init({
 	dsn: "https://bd8b19ac1d7245ceb7eb658dd7c61d9f@o851448.ingest.sentry.io/5818363",
 	integrations: [new Integrations.BrowserTracing()],
 	environment: process.env.NODE_ENV,
-	release: `${process.env.npm_package_name}@${process.env.npm_package_version}`,
+	release: `${packageJson.name}@${packageJson.version}`,
 
 	// Set tracesSampleRate to 1.0 to capture 100%
 	// of transactions for performance monitoring.
